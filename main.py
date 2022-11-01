aa1dc11c2b91ccfad18317bb075681ea193b1fcc
cristian23988 <cfbravo@unimayor.edu.co>
2022-10-31 20:35:59 -0500

    back
    Grabar y reproducir audio

--- main.py
+++ main.py
@@ -26,17 +26,61 @@ class Ventana(QMainWindow):
         self.button_home_teoria.clicked.connect(lambda: self.stackedWidget.setCurrentWidget(self.page_teoria))
         self.button_home_practicas.clicked.connect(lambda: self.stackedWidget.setCurrentWidget(self.page_practicas))
         self.button_home_quiz.clicked.connect(lambda: self.stackedWidget.setCurrentWidget(self.page_quiz))
+        #Grabar y reproducir audio-----------------------
+        self.button_practicas_record.clicked.connect(self.Grabar_Audio)
+        #-----------Especificar audio y ruta a reproductir
+        self.button_practicas_play.clicked.connect(self.Reproducir_Audio)
         #self.botonMidi.clicked.connect(self.xportMidi)
         #self.botonShowPartitura.clicked.connect(self.showPartitura)
         #self.botonStop.clicked.connect(self.stop)
         #self.botonStop.clicked.connect(self.converter_pdf_to_png())
 
-    def teoria(self):
+    def Abrir_Modulo_Teoria(self):
         print("teoria")        
-    def practicaSolfeo(self):
+    def Abrir_Modulo_Practica(self):
         print("practica")                    
-    def quiz(self):
+    def Abrir_Modulo_Quiz(self):
         print("quiz")        
+        
+    def Reproducir_Audio(self):
+        #pip uninstall playsound
+        #pip install playsound==1.2.2
+        from playsound import playsound  
+        #Definir Path de lectura (RUTA)
+        print("Reproduciendo...")
+        playsound('src/audio/audio_voz_natural.wav')
+        print("Finalizado.")
+
+    def Grabar_Audio(self):
+        import sounddevice as sd 
+        from scipy.io.wavfile import write 
+        import wavio as wv  
+        print('Grabando...')       
+        frequency = 44400        
+        duration = 10      
+        recording = sd.rec(int(duration * frequency), 
+                        samplerate = frequency, channels = 1)         
+        sd.wait()         
+        #write("recording0.wav", frequency, recording)  
+        #Graba en sonido monofonico, para cambiar a stereo va con channels = 2       
+        #definir RUTA de guardado
+        file_save='src/audio/voz_solfeo.wav' 
+        wv.write(file_save, recording, frequency, sampwidth=2)
+        print('Finalizado con exito')       
+        
+    def Convertir_Audio_A_MIDI(self):
+        import librosa
+        from sound_to_midi.monophonic import wave_to_midi
+        print("Starting...")
+        file_in = "Basepiano.wav"
+        file_out = "src/export_midi/estudiante/audio_estudiante.mid"
+        audio_data, srate = librosa.load(file_in, sr=None)
+        print("Audio file loaded!")
+        midi = wave_to_midi(audio_data, srate=srate)
+        print("Conversion finished!")
+        with open (file_out, 'wb') as file:
+            midi.writeFile(file)
+        print("Done. Exiting!")     
             
 
     def xportMidi(self):
